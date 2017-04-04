b5227fd5141fd1f84e35891c2f193a77a70eafed
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-04-04 16:44:57 +0100

    Initial checkin

---
+++ mancala.py
@@ -0,0 +1,124 @@
+#!/usr/local/bin/python3
+
+import operator
+import itertools
+
+class NotYourCup(Exception):
+  pass
+
+new_player = ((4,4,4,4,4,4),0)
+
+def repeating_moves(cups):
+  moves=[]
+  cup=0
+  for counters in cups:
+    if 6-cup==counters:
+      moves.append(cup)
+    cup+=1
+  return moves
+
+def move(cup, mine, yours):
+  my_cups, my_home = mine
+  your_cups, your_home = yours
+  if cup < 0 or cup > 5 or my_cups[cup] == 0:
+    return ((my_cups, my_home), (your_cups, your_home), True)
+  stones = my_cups[cup]
+  offset = cup + 1
+  board = list(my_cups) + [my_home] + list(your_cups)
+  board[cup] = 0
+  masks = []
+  while stones > 0:
+    m, stones = get_mask(stones, offset, len(board))
+    offset = 0
+    masks.append(m)
+  for m in masks:
+    board = [operator.add(a, b) for a, b in zip(board, m)]
+  again = m[6] == 1 and m[7] == 0
+  if not again:
+    last = 12 - len(list(itertools.takewhile(lambda a: a == 0, reversed(m))))
+    if last > -1 and last < 6 and board[last] == 1 and board[12 - last] > 0:
+      board[6] += board[last]
+      board[last] = 0
+      board[6] += board[12 - last]
+      board[12 - last] = 0
+  return ((tuple(board[:6]), board[6]), (tuple(board[7:]), your_home), again)
+
+def get_mask(stones, offset, size):
+  laying = min(stones, size - offset)
+  return ([0] * offset + [1] * laying + [0] * (size - laying - offset),
+          stones - laying)
+
+def steal(board, player, i):
+  if board[i] == 1 and i in player_cups[player] and board[opposite[i]] > 0:
+    board[home_index[player]] = board[home_index[player]] + 1 + \
+                                board[opposite[i]]
+    board[i] = 0
+    board[opposite[i]] = 0
+
+def is_game_over(mine, yours):
+  my_cups, my_home = mine
+  your_cups, your_home = yours
+  return all(map(lambda stones: stones == 0, my_cups)) or \
+         all(map(lambda stones: stones == 0, your_cups))
+
+def to_display(mine, yours):
+  my_cups, my_home = mine
+  your_cups, your_home = yours
+  return ['\t{5}\t{4}\t{3}\t{2}\t{1}\t{0}'.format(*your_cups),
+          '{0}\t\t\t\t\t\t\t{1}'.format(your_home, my_home),
+          '\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(*my_cups)]
+
+def score(player):
+  cups, home = player
+  return sum(cups) + home
+
+#TODO build legal moves tree
+# select leaf node with highest my_home
+# return move sequence as a list
+# or iterate, store, sort, choose
+def all_moves(mine, yours, prev_moves=[]):
+  my_cups, my_home = mine
+  for i in range(0, len(my_cups)):
+    if my_cups[i] > 0:
+      new_mine, new_yours, again = move(i, mine, yours)
+
+  
+
+def choose_move(mine, yours):
+  """Return the move that maximises my_home after this turn"""
+  my_cups, my_home = mine
+  valid_moves = [cup for cup in range(0, 6) if my_cups[cup] > 0]
+  move_results = [(move(cup, (my_cups, my_home), yours)[0][1], cup) 
+                  for cup in valid_moves]
+  return sorted(move_results)[-1][1]
+
+if __name__ == '__main__':
+  player_one = new_player
+  player_two = new_player
+  while not is_game_over(player_one, player_two):
+    again = True
+    while again and not is_game_over(player_one, player_two):
+      for line in to_display(player_one, player_two):
+        print(line)
+      print('\t(1)\t(2)\t(3)\t(4)\t(5)\t(6)')
+      cup = input("Player one's move:")
+      player_one, player_two, again = move(cup - 1, player_one, player_two)
+    again = True
+    while again and not is_game_over(player_one, player_two):
+      print('\t(6)\t(5)\t(4)\t(3)\t(2)\t(1)')
+      for line in to_display(player_one, player_two):
+        print(line)
+      cup = input("Player two's move:")
+      player_two, player_one, again = move(cup - 1, player_two, player_one)
+  print("Game over")
+  for line in to_display(player_one, player_two):
+    print(line)
+  player_one_score = score(player_one)
+  player_two_score = score(player_two)
+  if player_one_score == player_two_score:
+    print("It's draw!")
+  else:
+    prin( "Player {0} won [{1}:{2}]".format(
+                                    'one' if player_one_score > player_two_score 
+                                          else 'two',
+                                    player_one_score, player_two_score))
---
+++ test_mancala.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+from mancala import new_player, move, to_display, is_game_over, choose_move, \
+  repeating_moves, all_moves
+from unittest import TestCase, main
+
+class TestMove(TestCase):
+  def test_empty_cup(self):
+    self.assertEqual((((4,4,0,4,4,4),0), new_player, True),
+                     move(2,((4,4,0,4,4,4),0), new_player))
+
+  def test_not_your_cup_low(self):
+    self.assertEqual((new_player, new_player, True),
+                     move(-1, new_player, new_player))
+
+  def test_not_your_cup_high(self):
+    self.assertEqual((new_player, new_player, True),
+                     move(8, new_player, new_player))
+
+  def test_before_home(self):
+    self.assertEqual((((4,0,5,5,5,5),0),((4,4,4,4,4,4),0),False),
+                     move(1, new_player, new_player))
+
+  def test_home(self):
+    self.assertEqual((((4,4,0,5,5,5),1),((4,4,4,4,4,4),0),True),
+                     move(2, new_player, new_player))
+
+  def test_passed_home(self):
+    b = [4,4,6,4,4,4,0,4,4,4,4,4,4,0]
+    self.assertEqual((((4,4,0,5,5,5),1),((5,5,4,4,4,4),0),False),
+                     move(2, ((4,4,6,4,4,4),0), new_player))
+
+  def test_passed_opponent_home_move(self):
+    b = [4,4,12,4,4,4,0,4,4,4,4,4,4,0]
+    self.assertEqual((((5,5,0,5,5,5),1),((5,5,5,5,5,5),0),False),
+                     move(2, ((4,4,12,4,4,4),0), new_player))
+
+  def test_empty_cup_steal(self):
+    b = [4,4,4,4,4,0,0,4,4,4,4,4,4,0]
+    self.assertEqual((((4,0,5,5,5,0),5),((0,4,4,4,4,4),0),False),
+                     move(1, ((4,4,4,4,4,0),0),new_player))
+
+
+class TestIsGameOver(TestCase):
+  def test_game_not_over(self):
+    self.assertFalse(is_game_over(new_player, 
+                                          new_player))
+
+  def test_game_over_me(self):
+    self.assertTrue(is_game_over(((0,0,0,0,0,0),10),((1,0,0,0,0,0),12)))
+
+  def test_game_over_you(self):
+    self.assertTrue(is_game_over(((0,0,0,1,0,0),10),((0,0,0,0,0,0),12)))
+
+
+class TestToDisplay(TestCase):
+  def test_to_display(self):
+    self.assertEqual(['\t12\t11\t10\t9\t8\t7',
+                      '20\t\t\t\t\t\t\t10',
+                      '\t1\t2\t3\t4\t5\t6'],
+                     to_display(((1,2,3,4,5,6),10),
+                                        ((7,8,9,10,11,12),20)))
+
+
+class TestAllMoves(TestCase):
+  def test_one_valid_move(self):
+    self.assertEqual([[3]], all_moves(((0,0,0,1,0,0),0),
+                                            new_player))
+
+  def test_one_scoring_move(self):
+    self.assertEqual([[0],[3],[4]], all_moves(((1,0,0,4,1,0),0),
+                                            ((0,0,0,0,0,1),0)))
+
+  def test_two_scoring_moves(self):
+    self.assertEqual([[0],[3],[5]], all_moves(((1,0,0,4,1,0),0),
+                                            new_player))
+
+  def test_repeating_move(self):
+    self.assertEqual([[0],[3],[5,4,5,3,6]],
+                     all_moves(((1,0,0,3,2,1),0),
+                                          ((0,0,0,0,0,1),0)))
+
+
+class TestChooseMove(TestCase):
+  def test_one_valid_move(self):
+    self.assertEqual(3, choose_move(((0,0,0,1,0,0),0),
+                                            new_player))
+
+  def test_one_scoring_move(self):
+    self.assertEqual(3, choose_move(((1,0,0,4,1,0),0),
+                                            ((0,0,0,0,0,1),0)))
+
+  def test_two_scoring_moves(self):
+    self.assertEqual(4, choose_move(((1,0,0,4,1,0),0),
+                                            new_player))
+
+  def test_repeating_move(self):
+    self.assertEqual([5,4,5,3,5],
+                     choose_move(((1,0,0,3,2,1),0),
+                                          ((0,0,0,0,0,1),0)))
+
+
+class TestRepeatMovePossible(TestCase):
+  def test_no_repeating_move(self):
+    self.assertEqual([],
+                     repeating_moves((1,2,3,4,5,6)))
+
+  def test_single_repeating_move(self):
+    self.assertEqual([5],
+                     repeating_moves((0,0,0,0,0,1)))
+    self.assertEqual([4],
+                     repeating_moves((0,0,0,0,2,0)))
+    self.assertEqual([3],
+                     repeating_moves((0,0,0,3,0,0)))
+    self.assertEqual([2],
+                     repeating_moves((0,0,4,0,0,0)))
+    self.assertEqual([1],
+                     repeating_moves((0,5,0,0,0,0)))
+    self.assertEqual([0],
+                     repeating_moves((6,0,0,0,0,0)))
+
+if __name__ == '__main__':
+  main()
