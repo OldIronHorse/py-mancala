c2aa78b823235e3064e33251108adec065272b97
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-04-06 16:40:34 +0100

    human vs computer extracted to seperate script

--- mancala.py
+++ mancala.py
@@ -100,35 +100,3 @@ def choose_move(mine, yours):
   #TODO tie-break for equal scores?
   return best_move_seq
 
-if __name__ == '__main__':
-  player = new_player
-  computer = new_player
-  while not is_game_over(player, computer):
-    again = True
-    while again and not is_game_over(player, computer):
-      print()
-      for line in to_display(player, computer):
-        print(line)
-      print('\t(1)\t(2)\t(3)\t(4)\t(5)\t(6)')
-      cup = int(input("Player's move:"))
-      player, computer, again = move(cup - 1, player, computer)
-
-    computer_move_seq=choose_move(computer, player)
-    print("Computer's move(s):{}".format([cup+1 for cup in computer_move_seq]))
-    for cup in computer_move_seq:
-      print('\t(6)\t(5)\t(4)\t(3)\t(2)\t(1)')
-      for line in to_display(player, computer):
-        print(line)
-      computer, player, again=move(cup, computer, player)
-  print("Game over")
-  for line in to_display(player, computer):
-    print(line)
-  player_score = score(player)
-  computer_score = score(computer)
-  if player_score == computer_score:
-    print("It's draw!")
-  else:
-    print( "Player {0} won [{1}:{2}]".format(
-                                    'one' if player_score > computer_score 
-                                          else 'two',
-                                    player_score, computer_score))
