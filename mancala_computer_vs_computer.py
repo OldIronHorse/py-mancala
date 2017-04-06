292504a87fd113f74365832e4e6feff2184b9c29
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-04-06 17:37:11 +0100

    computer vs computer play added.

--- mancala_computer_vs_computer.py
+++ mancala_computer_vs_computer.py
@@ -7,17 +7,19 @@ if __name__ == '__main__':
   player = new_player
   computer = new_player
   while not is_game_over(player, computer):
-    again = True
-    while again and not is_game_over(player, computer):
-      print()
-      for line in to_display(player, computer):
-        print(line)
-      print('\t(1)\t(2)\t(3)\t(4)\t(5)\t(6)')
-      cup = int(input("Player's move:"))
-      player, computer, again = move(cup - 1, player, computer)
-
+    print()
+    for line in to_display(player, computer):
+      print(line)
+    print('\t(1)\t(2)\t(3)\t(4)\t(5)\t(6)')
+    player_move_seq=choose_move(player,computer)
+    print("Player one's move(s):{}".format([cup+1 for cup in player_move_seq]))
+    for cup in player_move_seq:
+      player, computer, again = move(cup, player, computer)
+    if is_game_over(player, computer):
+      break
     computer_move_seq=choose_move(computer, player)
-    print("Computer's move(s):{}".format([cup+1 for cup in computer_move_seq]))
+    print(computer, player, computer_move_seq)
+    print("Player two's move(s):{}".format([cup+1 for cup in computer_move_seq]))
     for cup in computer_move_seq:
       print('\t(6)\t(5)\t(4)\t(3)\t(2)\t(1)')
       for line in to_display(player, computer):
@@ -31,7 +33,7 @@ if __name__ == '__main__':
   if player_score == computer_score:
     print("It's draw!")
   else:
-    print( "{0} won [{1}:{2}]".format(
-                                    'Human' if player_score > computer_score 
-                                          else 'Computer',
+    print( "Player {0} won [{1}:{2}]".format(
+                                    'one' if player_score > computer_score 
+                                          else 'two',
                                     player_score, computer_score))
